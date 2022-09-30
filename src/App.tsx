8e7680dd602201a2ff8e8537ea3cdec7ec5cbd22
Mehedi Hasan <mehedi08h@gmail.com>
2022-09-30 09:17:50 +0600

    setup

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+    return (
+        <div>
+            <h1 className="text-blue-600">Hello</h1>
+        </div>
+    );
+}
+
+export default App;
---
+++ src/components/index.ts
@@ -0,0 +1,4 @@
+import Banner from "./Banner";
+import Navbar from "./Navbar";
+
+export { Banner, Navbar };
